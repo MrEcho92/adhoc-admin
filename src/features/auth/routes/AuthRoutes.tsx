50a447ee56a15ecf4dc3875b92586d7589a4fe05
MrEcho92 <chigozie10@gmail.com>
2024-06-04 00:15:11 +0100

    add sign up page

--- src/features/auth/routes/AuthRoutes.tsx
+++ src/features/auth/routes/AuthRoutes.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import LoginPage from "../page/LoginPage";
 
 export function AuthRoutes() {
   return (
     <Routes>
-      <Route path="/*" element={<div>login page</div>} />
-      <Route path="login" element={<div>login page</div>} />
-      <Route path="signup" element={<div>Sign page</div>} />
+      <Route path="/*" element={<LoginPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<div>Sign page</div>} />
     </Routes>
   );
 }
